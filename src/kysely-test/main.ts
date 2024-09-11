9a16548d5229a62eea7585c0eedbf88e1d14abfd
Mingsterism <ming.k@hotmail.com>
2024-09-11 17:08:11 +0800

    add in advanced selects and filters

--- src/kysely-test/main.ts
+++ src/kysely-test/main.ts
@@ -1,4 +1,4 @@
-import { Kysely, PostgresDialect } from "kysely";
+import { Kysely, PostgresDialect, sql } from "kysely";
 import { DB } from "kysely-codegen";
 import { Pool } from "pg";
 
@@ -9,12 +9,48 @@ const db = new Kysely<DB>({
     }),
   }),
 });
+async function advancedQuery(
+  businessCode: string,
+  creditTerm?: number,
+  description?: string,
+) {
+  const result = await db
+    .with("filtered_business", (qb) =>
+      qb
+        .withSchema("finance")
+        .selectFrom("business_profiles")
+        .where("business_profiles.code", "=", businessCode)
+        .select("id"),
+    )
+    .withSchema("finance")
+    .selectFrom("generic_transactions")
+    .leftJoin(
+      "filtered_business",
+      "filtered_business.id",
+      "generic_transactions.business_profile_id",
+    )
+    .selectAll("generic_transactions")
+    .$if(Boolean(creditTerm), (qb) =>
+      qb.where("generic_transactions.amount", ">=", "12"),
+    )
+    .$if(Boolean(description), (qb) =>
+      qb.where(
+        sql`lower(generic_transactions.description) like ${`%${description!.toLowerCase()}%`}`,
+      ),
+    )
+    .execute();
+
+  console.log("Advanced query result:", result);
+  return result;
+}
 
 async function main() {
-  const creditTerm = true;
-  const description =
-    "deposit transaction at Wilderman - Legros using card ending with ***(...8555) for RON 599.35 in account ***24141293";
+  const creditTerm = 28;
+  const description = "";
+  const businessCode = "T20001";
+
   console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
+
   let transactionsQuery = db
     .withSchema("finance")
     .selectFrom("generic_transactions")
@@ -23,18 +59,27 @@ async function main() {
       "business_profiles.id",
       "generic_transactions.business_profile_id",
     )
+    .where("business_profiles.code", "ilike", `%${businessCode}%`)
     .selectAll();
 
   if (creditTerm) {
-    transactionsQuery = transactionsQuery.where((eb) =>
-      eb.and([eb("credit_term", ">=", 28)]),
+    transactionsQuery = transactionsQuery.where(
+      "credit_term",
+      ">=",
+      creditTerm,
     );
   }
-  const transactions = await transactionsQuery
-    .where((eb) => eb("generic_transactions.description", "ilike", description))
-    .limit(5)
-    .execute();
-  console.log("tranasctions", transactions);
+
+  if (description) {
+    transactionsQuery = transactionsQuery.where(
+      sql`lower(generic_transactions.description) like ${`%${description.toLowerCase()}%`}`,
+    );
+  }
+
+  const transactions = await transactionsQuery.limit(5).execute();
+  console.log("transactions", transactions);
+  const result = await advancedQuery("DT1-00002", 28, "some description");
+  console.log("result advaned query: ", result);
 }
 
 main();
